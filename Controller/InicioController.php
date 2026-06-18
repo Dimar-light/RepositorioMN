9093892963e4d80056e52180a6fff1de40f4e5fb
Dimar-light <margelinearce1981@gmail.com>
2026-06-17 22:40:42 -0600

    Esto es de semana 5

--- Controller/InicioController.php
+++ Controller/InicioController.php
@@ -1,5 +1,25 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/RepoMN/Model/InicioModel.php';
 
 if (isset($_POST["btnRegistrar"])) {
-    echo $_POST["identificacion"];
+    $identificacion = $_POST["identificacion"];
+    $nombre = $_POST["nombre"];
+    $correoElectronico = $_POST["correoElectronico"];
+    $contrasenna = $_POST["contrasenna"];
+
+    RegistrarUsuarioModel($identificacion, $nombre, $correoElectronico, $contrasenna);
+}
+
+if (isset($_POST["btnIniciarSesion"])) {
+    $identificacion = $_POST["identificacion"];
+    $contrasenna = $_POST["contrasenna"];
+
+    $datos = IniciarSesionModel($identificacion, $contrasenna);
+
+    if ($datos) {
+        header("Location: ../../View/vInicio/Principal.php");
+        exit();
+    }
+
+    $_POST["Mensaje"] = "NO SE AUTENTICÓ CORRECTAMENTE";
 }
